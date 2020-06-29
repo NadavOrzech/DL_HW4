971d06e0976c9dddffd9b539eadfb2f1a9d8451d
NadavOrzech <45461272+NadavOrzech@users.noreply.github.com>
2020-06-29 17:24:52 +0300

    final training

---
+++ TMPanswers.py
@@ -0,0 +1,95 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+
+# ==============
+# Part 1 answers
+
+
+def part1_pg_hyperparams():
+    hp = dict(batch_size=32,
+              gamma=0.99,
+              beta=0.5,
+              learn_rate=1e-3,
+              eps=1e-8,
+              )
+    # TODO: Tweak the hyperparameters if needed.
+    #  You can also add new ones if you need them for your model's __init__.
+    # ====== YOUR CODE: ======
+    hp['beta'] = 0.1
+    # ========================
+    return hp
+
+
+def part1_aac_hyperparams():
+    hp = dict(batch_size=32,
+              gamma=0.99,
+              beta=1.,
+              delta=1.,
+              learn_rate=1e-3,
+              eps=1e-8,
+              )
+    # TODO: Tweak the hyperparameters. You can also add new ones if you need
+    #   them for your model implementation.
+    # ====== YOUR CODE: ======
+    hp['gamma'] = 0.95
+    hp['delta'] = 0.5
+    hp['beta'] = 0.75
+    hp['learn_rate'] = 3e-3
+
+    # ========================
+    return hp
+
+
+part1_q1 = r"""
+**Your answer:
+The policy gradient technic produces high variance, which as explained in the exercise causes erratic optimization
+behavior and slow convergence. This happens because when we compute the gradient we sample trajectories from the current
+policy and average their values thus making the computed gradient dependant on the randomly sampled trajectories. 
+Subtracting an appropriate baseline we will cause the expectation to be clustered closer to zero, which will cause the
+variance to drop.
+
+For example in our game, lets say that the actions probabilities are [0.4, 0.1, 0.3, 0.2] and the reward for the four 
+trajectories are [1000, 1001, 1003, 1010]. The variance for this parameters is 2.9 * 10e8. If we will choose baseline of
+1003 we will calculate variance for {0.4*-3, 0.1*-2, 0.5*0, 0.2*7} which value is 0.413. As we explained the variance 
+dropped dramatically and as a result improves convergence.
+**
+
+
+
+
+"""
+
+
+part1_q2 = r"""
+In AAC, when using the estimated q-values as regression targets for our state-values, why do we get a valid 
+approximation? Hint: how is  𝑣𝜋(𝑠)  expressed in terms of  𝑞𝜋(𝑠,𝑎) ?
+
+**Your answer:
+As we learned the advantage function is the difference between the action-value function and the state-values function.
+The state-value represents the value of the state by averaging over all the actions possible according to the policy. 
+The action-value represents the value of the state according to a given the first action.
+So we can see that the state-values are an average of the action-values of the same state, so by using the estimated 
+q-values as regression targets we can estimate the average action-values thus calculate the state-values.
+**
+
+
+"""
+
+
+part1_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
---
+++ TMPrl_ac.py
@@ -0,0 +1,130 @@
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional
+
+from .rl_pg import PolicyAgent, TrainBatch, VanillaPolicyGradientLoss
+
+
+class AACPolicyNet(nn.Module):
+    def __init__(self, in_features: int, out_actions: int, **kw):
+        """
+        Create a model which represents the agent's policy.
+        :param in_features: Number of input features (in one observation).
+        :param out_actions: Number of output actions.
+        :param kw: Any extra args needed to construct the model.
+        """
+        super().__init__()
+        # TODO:
+        #  Implement a dual-head neural net to approximate both the
+        #  policy and value. You can have a common base part, or not.
+        # ====== YOUR CODE: ======
+        self.actor = nn.Sequential(
+            nn.Linear(in_features, 64),
+            nn.Tanh(),
+            nn.Linear(64, 128),
+            nn.Tanh(),
+            nn.Linear(128, out_actions),
+        )
+        self.critic = nn.Sequential(
+            nn.Linear(in_features, 64),
+            nn.Tanh(),
+            nn.Linear(64, 128),
+            nn.Tanh(),
+            nn.Linear(128, 1),
+        )
+        # ========================
+
+    def forward(self, x):
+        """
+        :param x: Batch of states, shape (N,O) where N is batch size and O
+        is the observation dimension (features).
+        :return: A tuple of action values (N,A) and state values (N,1) where
+        A is is the number of possible actions.
+        """
+        # TODO:
+        #  Implement the forward pass.
+        #  calculate both the action scores (policy) and the value of the
+        #  given state.
+        # ====== YOUR CODE: ======
+        action_scores = self.actor(x)
+        state_values = self.critic(x)
+        # ========================
+
+        return action_scores, state_values
+
+    @staticmethod
+    def build_for_env(env: gym.Env, device='cpu', **kw):
+        """
+        Creates a A2cNet instance suitable for the given environment.
+        :param env: The environment.
+        :param kw: Extra hyperparameters.
+        :return: An A2CPolicyNet instance.
+        """
+        # TODO: Implement according to docstring.
+        # ====== YOUR CODE: ======
+        env_actions = env.action_space.n
+        # env_actions = 4
+        net = AACPolicyNet(env.observation_space.shape[0],env_actions,**kw)
+        # ========================
+        return net.to(device)
+
+
+class AACPolicyAgent(PolicyAgent):
+
+    def current_action_distribution(self) -> torch.Tensor:
+        # TODO: Generate the distribution as described above.
+        # ====== YOUR CODE: ======
+        actions_scores = self.p_net(self.curr_state)[0]
+        actions_proba = nn.functional.softmax(actions_scores, dim=0)
+        # ========================
+        return actions_proba
+
+
+class AACPolicyGradientLoss(VanillaPolicyGradientLoss):
+    def __init__(self, delta: float):
+        """
+        Initializes an AAC loss function.
+        :param delta: Scalar factor to apply to state-value loss.
+        """
+        super().__init__()
+        self.delta = delta
+
+    def forward(self, batch: TrainBatch, model_output, **kw):
+
+        # Get both outputs of the AAC model
+        action_scores, state_values = model_output
+
+        # TODO: Calculate the policy loss loss_p, state-value loss loss_v and
+        #  advantage vector per state.
+        #  Use the helper functions in this class and its base.
+        # ====== YOUR CODE: ======
+        advantage = self._policy_weight(batch, state_values)
+        loss_v = self._value_loss(batch, state_values)
+        loss_p = self._policy_loss(batch, action_scores, advantage)
+        # ========================
+
+        loss_v *= self.delta
+        loss_t = loss_p + loss_v
+        return loss_t, dict(loss_p=loss_p.item(), loss_v=loss_v.item(),
+                            adv_m=advantage.mean().item())
+
+    def _policy_weight(self, batch: TrainBatch, state_values: torch.Tensor):
+        # TODO:
+        #  Calculate the weight term of the AAC policy gradient (advantage).
+        #  Notice that we don't want to backprop errors from the policy
+        #  loss into the state-value network.
+        # ====== YOUR CODE: ======
+        q_vals = batch.q_vals
+        advantage = q_vals - state_values
+        # ========================
+        return advantage
+
+    def _value_loss(self, batch: TrainBatch, state_values: torch.Tensor):
+        # TODO: Calculate the state-value loss.
+        # ====== YOUR CODE: ======
+        q_vals = batch.q_vals
+        loss_v = nn.functional.mse_loss(q_vals, state_values)
+        # ========================
+        return loss_v
+
